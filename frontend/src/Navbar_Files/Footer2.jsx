511c57b57d67fa4a0c411533f5d410b688601048
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-05-19 07:31:03 +0100

    adjust navbar

--- frontend/src/Navbar_Files/Footer2.jsx
+++ frontend/src/Navbar_Files/Footer2.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer2 = () => {
     return (
-        <footer className="bg-black text-white py-3 fixed bottom-0 left-0 right-0">
-            <div className="container mx-auto text-center">
-                <p>&copy; {new Date().getFullYear()} LearnLodge. All rights reserved.</p>
+        <footer className="bg-black text-white py-2 fixed bottom-0 left-0 right-0">
+            <div className="container mx-auto text-center text-[0.85rem]">
+                <p>&copy; {new Date().getFullYear()} LearnLodge</p>
                 <p><i>Learn Anytime Anywhere </i> 🎓🕜🌐</p>
             </div>
         </footer>
